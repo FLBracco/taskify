58c7ba3154eda0230a333c31f6f35fe1897a0af4
Bracco Fabricio <braccof.0601@gmail.com>
2025-05-15 17:08:54 -0300

    feat:(register-login): Se creo get de prueba para corroborar conexion con la db.

---
+++ src/routes/register.routes.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import pool from '../db/db.config';
+
+const userCreateRoutes = express.Router();
+
+// Probando que se conecte a la db
+userCreateRoutes.get('/test', async (_req: Request, res: Response)=>{
+    try {
+        const getUserQuery = `
+            SELECT * FROM users;
+        `
+        const result = await pool.query(getUserQuery);
+        res.status(200).send(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Error al obtener los usuarios ", err});
+    }
+})
+
+export default userCreateRoutes;
