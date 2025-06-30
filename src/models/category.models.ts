6fb44ed32e47aa67e7ea9df1186752e89ecbe570
Bracco Fabricio <braccof.0601@gmail.com>
2025-06-30 12:22:15 -0300

    feat(tasks) WIP: Arranco el trabajo de las tareas, empezando por las categorias de las misma

---
+++ src/models/category.models.ts
@@ -0,0 +1,7 @@
+import z from 'zod';
+
+const CategorySchema = z.object({
+    name: z.string({required_error: 'name is required'}).trim().min(1).max(100)
+})
+
+export type CategoryType = z.infer<typeof CategorySchema>;
